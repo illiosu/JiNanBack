825f730cef7b4a42335e379afa8a26eccce10ca7
illiosu <2500509095@qq.com>
2024-04-07 16:46:32 +0800

    init

---
+++ src/test/java/com/example/jinan/JiNanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jinan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiNanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
